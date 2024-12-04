f9c82cd4498df48053a9916caf2261baaa0740ef
Aerone123 <kwang321.as@gmail.com>
2024-12-04 21:14:07 +0700

    Penambahan Dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Carbon\Carbon;
+use App\Models\Transaksi;
 
 class HomeController extends Controller
 {
-        /**
+    /**
      * Create a new controller instance.
      *
      * @return void
@@ -23,6 +25,44 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('pages.dashboard');
+        $currentMonth = Carbon::now()->month;
+        $currentYear = Carbon::now()->year;
+
+        $data['transaksiBulanIni'] = Transaksi::whereYear('tanggal', $currentYear)
+            ->whereMonth('tanggal', $currentMonth)
+            ->count();
+
+        $data['penjualanNow'] = Transaksi::whereYear('tanggal', $currentYear)
+            ->whereMonth('tanggal', $currentMonth)
+            ->sum('total_pembayaran');
+
+        $previousMonth = Carbon::now()->subMonth()->month;
+        $previousYear = Carbon::now()->subMonth()->year;
+
+        $transaksiBulanLalu = Transaksi::whereYear('tanggal', $previousYear)
+            ->whereMonth('tanggal', $previousMonth)
+            ->count();
+
+        if ($transaksiBulanLalu > 0) {
+            $percentageChange = (($data['transaksiBulanIni'] - $transaksiBulanLalu) / $transaksiBulanLalu) * 100;
+        } else {
+            $percentageChange = $data['transaksiBulanIni'] > 0 ? 100 : 0;
+        }
+
+        $data['percentageChange'] = $percentageChange;
+
+        $penjualanThen = Transaksi::whereYear('tanggal', $previousYear)
+            ->whereMonth('tanggal', $previousMonth)
+            ->sum('total_pembayaran');
+
+        if ($penjualanThen > 0) {
+            $percentageChangePenjualan = (($data['penjualanNow'] - $penjualanThen) / $penjualanThen) * 100;
+        } else {
+            $percentageChangePenjualan = $data['penjualanNow'] > 0 ? 100 : 0;
+        }
+
+        $data['percentageChangePenjualan'] = $percentageChangePenjualan;
+        
+        return view('pages.dashboard', $data);
     }
 }
